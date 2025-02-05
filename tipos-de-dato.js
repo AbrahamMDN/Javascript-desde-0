6c6372311b54544c105970508fb0545e061205ac
Abraham Medina <abrahammdnlpz@gmail.com>
2025-02-04 20:36:15 -0600

    Lección 1 Campus

---
+++ tipos-de-dato.js
@@ -0,0 +1,33 @@
+// Este es un comentario en JS
+//colsole.log es una función que se utiliza para imprimir en consola
+
+console.log("hola mundo");
+
+//Declaracion de mi variable
+var nombre = "Angel"; //string
+var edad = 23; //Number
+var sueldo = 1.235; //Number
+var casado = false; //Boolean
+var vacunas = true; //Boolean
+var hijos = null; //Null
+var dato; //Indefined
+
+console.log(typeof(nombre));
+//La semantica es importante, significado de las palabras
+//Si ponemos typeof de null dira que es un objeto, pero es ausencia intencional de valor.
+//Esto es un error por compatibilidad de versiones
+console.log(10*"Angel");
+
+/* Esto es un comentario en bloque
+o comentarios multilinea */
+
+//Ejercicios Campus
+console.log(typeof(42));
+console.log(typeof("Veinticinco"));
+console.log(typeof(-666));
+console.log(typeof(true));
+console.log(typeof(0));
+console.log(typeof(""));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof(FALSE));
