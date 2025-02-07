d40023507e623ea60743cccdc0675584688d0468
Abraham Medina <abrahammdnlpz@gmail.com>
2025-02-06 18:59:34 -0600

    Clase 2: Variables

---
+++ variables.js
@@ -0,0 +1,14 @@
+//var es una forma antigua de declarar variables
+
+var nombre = "Ana";
+
+var nombre = "Fernanda";
+
+var nombre = "Victor";
+
+//let no permite redeclarar variables
+//let tiene scope de bloque, lo que significa que solo vive en el bloque donde fue delcarado
+
+console.log(nombre);
+
+let apellido = "Gutierrez";
